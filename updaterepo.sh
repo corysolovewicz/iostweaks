80d5fe41eec4afbdf396442d1e5464d19a20d618
csolovewicz <csolovewicz@netspi.com>
2024-03-12 16:41:09 -0700

    updating names

--- updaterepo.sh
+++ updaterepo.sh
@@ -31,7 +31,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then # Linux usage of repo.me
     
     rm {Packages{,.xz,.gz,.bz2,.zst},Release{,.gpg}} 2> /dev/null
     
-    ./apt-ftparchive packages ./debians > Packages
+    ./apt-ftparchive packages ./deb > Packages
     gzip -c9 Packages > Packages.gz
     xz -c9 Packages > Packages.xz
     zstd -c19 Packages > Packages.zst
@@ -71,19 +71,19 @@ if [[ "$OSTYPE" == "linux"* ]]; then # Linux usage of repo.me
     elif [[ "$(uname)" == Darwin ]] && [[ "$(uname -p)" != i386 ]]; then # iOS/iPadOS usage of repo.me
     cd "$(dirname "$0")" || exit
     echo "Checking for apt-ftparchive..."
-    if test ! "$(apt-ftparchive)"; then
+    if test ! "$(./apt-ftparchive)"; then
         apt update && apt install apt-utils -y
     fi
 
     rm {Packages{,.xz,.gz,.bz2,.zst},Release{,.gpg}} 2> /dev/null
 
-    apt-ftparchive packages ./debians > Packages
+    ./apt-ftparchive packages ./debians > Packages
     gzip -c9 Packages > Packages.gz
     xz -c9 Packages > Packages.xz
     zstd -c19 Packages > Packages.zst
     bzip2 -c9 Packages > Packages.bz2
 
-    apt-ftparchive release -c ./assets/repo/repo.conf . > Release
+    ./apt-ftparchive release -c ./assets/repo/repo.conf . > Release
 
     echo "Repository Updated, thanks for using repo.me!"
 else
